66a3ef0ebefba066ac67e01383ca5c5f103e03c4
Manmeet Moudgill <manmeetmoudgill76@gmail.com>
2021-06-15 12:51:37 +0200

    Done with Left side Bar

--- src/components/CenterPartDiv.vue
+++ src/components/CenterPartDiv.vue
@@ -3,23 +3,164 @@
         <CenterNavbar />
         <div class="container-fluid d-flex">
 
-        <h4 class="text-primary">Center Part div</h4>
+
+<!-- first div -->
+        <CenterDivComponent>
+            <div class="container container-first-image">
+                <img class="image-first-div" src="../assets/yoga.jpg" alt="Yoga">
+            </div>
+
+            <!-- second container
+             -->
+            <div class="container-fluid d-flex align-items-center container-second">
+                <img id="img" class=" logo style-scope yt-img-shadow" alt="Avatar image" height="40" width="40" src="https://yt3.ggpht.com/yti/ANoDKi6Q83GGUGLCthNtL56hyUE78X-IlTf0yxaKgA=s88-c-k-c0x00ffffff-no-rj-mo">
+               <div class=" container-fluid d-flex justify-content-start flex-column">
+                   <div class="upperDiv">
+                    <h5 class="text-dark">Bodybuilding show</h5>
+
+                   </div>
+                   <div class="lowerDiv d-flex flex-column align-items-center justify-content-start">
+
+                    <h6 class="font-size text-dark mx-2 text-left">Manmeet Vlogs</h6>
+                    <div class="container d-flex">
+                     <h6 class="font-size text-dark mx-1">1M Views °</h6>
+                     <h6 class="font-size text-dark  ">2 Days Ago</h6>
+                    </div>
+                   </div>
+                </div>
+              
+            </div>
+        </CenterDivComponent>  
+
+
+     <!-- Second Div -->
+     
+        <CenterDivComponent>
+            <div class="container container-first-image">
+                <img class="image-first-div" src="../assets/yoga.jpg" alt="Yoga">
+            </div>
+
+            <!-- second container
+             -->
+            <div class="container-fluid d-flex align-items-center container-second">
+                <img id="img" class=" logo style-scope yt-img-shadow" alt="Avatar image" height="40" width="40" src="https://yt3.ggpht.com/yti/ANoDKi6Q83GGUGLCthNtL56hyUE78X-IlTf0yxaKgA=s88-c-k-c0x00ffffff-no-rj-mo">
+               <div class=" container-fluid d-flex justify-content-start flex-column">
+                   <div class="upperDiv">
+                    <h5 class="text-dark">Bodybuilding show</h5>
+
+                   </div>
+                   <div class="lowerDiv d-flex flex-column align-items-center justify-content-start">
+
+                    <h6 class="font-size text-dark mx-2 text-left">Manmeet Vlogs</h6>
+                    <div class="container d-flex">
+                     <h6 class="font-size text-dark mx-1">1M Views °</h6>
+                     <h6 class="font-size text-dark  ">2 Days Ago</h6>
+                    </div>
+                   </div>
+                </div>
+              
+            </div>
+        </CenterDivComponent>  
+
+
+   <!-- Third Div -->
+   
+        <CenterDivComponent>
+            <div class="container container-first-image">
+                <img class="image-first-div" src="../assets/yoga.jpg" alt="Yoga">
+            </div>
+
+            <!-- second container
+             -->
+            <div class="container-fluid d-flex align-items-center container-second">
+                <img id="img" class=" logo style-scope yt-img-shadow" alt="Avatar image" height="40" width="40" src="https://yt3.ggpht.com/yti/ANoDKi6Q83GGUGLCthNtL56hyUE78X-IlTf0yxaKgA=s88-c-k-c0x00ffffff-no-rj-mo">
+               <div class=" container-fluid d-flex justify-content-start flex-column">
+                   <div class="upperDiv">
+                    <h5 class="text-dark">Bodybuilding show</h5>
+
+                   </div>
+                   <div class="lowerDiv d-flex flex-column align-items-center justify-content-start">
+
+                    <h6 class="font-size text-dark mx-2 text-left">Manmeet Vlogs</h6>
+                    <div class="container d-flex">
+                     <h6 class="font-size text-dark mx-1">1M Views °</h6>
+                     <h6 class="font-size text-dark  ">2 Days Ago</h6>
+                    </div>
+                   </div>
+                </div>
+              
+            </div>
+        </CenterDivComponent>       
+    <!-- Fourth Div -->
+
+        <CenterDivComponent>
+            <div class="container container-first-image">
+                <img class="image-first-div" src="../assets/yoga.jpg" alt="Yoga">
+            </div>
+
+            <!-- second container
+             -->
+            <div class="container-fluid d-flex align-items-center container-second">
+                <img id="img" class=" logo style-scope yt-img-shadow" alt="Avatar image" height="40" width="40" src="https://yt3.ggpht.com/yti/ANoDKi6Q83GGUGLCthNtL56hyUE78X-IlTf0yxaKgA=s88-c-k-c0x00ffffff-no-rj-mo">
+               <div class=" container-fluid d-flex justify-content-start flex-column">
+                   <div class="upperDiv">
+                    <h5 class="text-dark">Bodybuilding show</h5>
+
+                   </div>
+                   <div class="lowerDiv d-flex flex-column align-items-center justify-content-start">
+
+                    <h6 class="font-size text-dark mx-2 text-left">Manmeet Vlogs</h6>
+                    <div class="container d-flex">
+                     <h6 class="font-size text-dark mx-1">1M Views °</h6>
+                     <h6 class="font-size text-dark ">2 Days Ago</h6>
+                    </div>
+                   </div>
+                </div>
+              
+            </div>
+        </CenterDivComponent>    
+        
         </div>
     </div>
 
 </template>
 <script>
 import CenterNavbar from './CenterNavbar.vue'
+import CenterDivComponent from './CenterPartDivComponents/CenterDivComponent.vue'
 export default {
     name:'CenterPartDiv',
     components:{
-        CenterNavbar
+        CenterNavbar,
+        CenterDivComponent
     }
 }
 </script>
 <style scoped>
-.CenterPartDiv{
-    border: 2px solid green;
+
+.image-first-div{
+    width: 250;
+    height: 200px;
+}
+.container-first-image{
+    padding-left: 0px;
     
+}
+.container-second{
+   
+   
+    padding-left: 17px;
+    padding-top: 11px;
+}
+.logo{
+    border-radius: 20px;
+}
+.upperDiv{
+   
+    width: 200px;
+
+}
+.lowerDiv{
+    
+    width: 200px;
 }
 </style>
---
+++ src/components/CenterPartDivComponents/CenterDivComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container d-flex flex-wrap flex-column my-2  MainDiv">
+        <slot></slot>
+
+    </div>
+</template>
+
+<script>
+export default {
+   name:'CenterDivComponent' 
+}
+</script>
+
+<style scoped>
+
+</style>
