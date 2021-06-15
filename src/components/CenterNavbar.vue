0f1a366e90e01f585c3a73a20e6cf0e9d4e51192
Manmeet Moudgill <manmeetmoudgill76@gmail.com>
2021-06-15 11:52:02 +0200

    Done with Left side Bar

---
+++ src/components/CenterNavbar.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container-fluid flex-wrap bord-div d-flex justify-content-between my-1 ">
+<hr class="lined" >
+  <div class="container-fluid d-flex flex-wrap justify-content-between align-items-center">
+       <button class="btn-lg btn-light border-radius-sm">American Idol</button>
+        <button class="btn-lg btn-light border-radius-sm">Indian Idol</button>
+        <button class="btn-lg btn-light border-radius-sm">Tv Serials</button>
+        <button class="btn-lg btn-light border-radius-sm">American Idol</button>
+        <button class="btn-lg btn-light border-radius-sm">Indian Idol</button>
+        <button class="btn-lg btn-light border-radius-sm">Tv Serials</button>
+        <button class="btn-lg btn-light border-radius-sm">American Idol</button>
+        <button class="btn-lg btn-light border-radius-sm">Indian Idol</button>
+        <button class="btn-lg btn-light border-radius-sm">Tv Serials</button>
+  </div>
+  <hr class="lined">
+    </div>
+</template>
+<script>
+export default {
+    name:'CenterNavbar'
+}
+</script>
+<style scoped>
+.border-radius-sm{
+    border-radius: 80px;
+}
+.bord-div{
+    padding-top: 8px;
+    padding-bottom: 8px;
+    border-top: 1px solid darkgray;
+    border-bottom: 1px solid darkgray;
+}
+</style>
--- src/components/CenterPartDiv.vue
+++ src/components/CenterPartDiv.vue
@@ -1,12 +1,20 @@
 <template>
     <div class="container-fluid flex-grow-1 CenterPartDiv">
+        <CenterNavbar />
+        <div class="container-fluid d-flex">
+
         <h4 class="text-primary">Center Part div</h4>
+        </div>
     </div>
 
 </template>
 <script>
+import CenterNavbar from './CenterNavbar.vue'
 export default {
-    name:'CenterPartDiv'
+    name:'CenterPartDiv',
+    components:{
+        CenterNavbar
+    }
 }
 </script>
 <style scoped>
